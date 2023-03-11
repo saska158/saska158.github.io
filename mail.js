8eec222da056a71ec3a489ad7c0215c606798801
Saska <mikic.saska@gmail.com>
2023-03-11 12:52:01 +0100

    Update mail.js

--- mail.js
+++ mail.js
@@ -3,11 +3,9 @@ const mailTooltip = document.querySelector(".mail-tooltip")
 mail.addEventListener("click", () => {
     navigator.clipboard.writeText("mikic.saska@gmail.com")
     mailTooltip.style.opacity = 1
-   // mailTooltip.style.visibility = 'visible'
     
     setTimeout(() => {
         mailTooltip.style.opacity = 0
-      //  mailTooltip.style.visibility = 'hidden'
     }, 3000)
 })
 
